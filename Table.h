e00ad2b6f9bfc2a3d39726c9d19b4db8bb131385
dany12day <57262666+dany12day@users.noreply.github.com>
2019-11-15 13:21:09 +0200

    Update Table.h
    compleatre piese albe

--- Table.h
+++ Table.h
@@ -9,23 +9,23 @@ public:
 			for (int j = 0; i < 8; j++)
 				dimensions[i][j] = nullptr;
 
-		dimensions[0][0] = new Rook;
-		dimensions[0][1] = new Knight;
-		dimensions[0][2] = new Bishop;
-		dimensions[0][3] = new Queen;
-		dimensions[0][4] = new King;
-		dimensions[0][5] = new Bishop;
-		dimensions[0][6] = new Knight;
-		dimensions[0][7] = new Rook;
+		dimensions[1][0] = new Pawn('P', 1, 0, 1);
+		dimensions[1][1] = new Pawn('P', 1, 1, 1);
+		dimensions[1][2] = new Pawn('P', 1, 2, 1);
+		dimensions[1][3] = new Pawn('P', 1, 3, 1);
+		dimensions[1][4] = new Pawn('P', 1, 4, 1);
+		dimensions[1][5] = new Pawn('P', 1, 5, 1);
+		dimensions[1][6] = new Pawn('P', 1, 6, 1);
+		dimensions[1][7] = new Pawn('P', 1, 7, 1);
 
-		dimensions[1][0] = new Pawn;
-		dimensions[1][1] = new Pawn;
-		dimensions[1][2] = new Pawn;
-		dimensions[1][3] = new Pawn;
-		dimensions[1][4] = new Pawn;
-		dimensions[1][5] = new Pawn;
-		dimensions[1][6] = new Pawn;
-		dimensions[1][7] = new Pawn;
+		dimensions[0][0] = new Rook('R', 0, 0, 1);
+		dimensions[0][1] = new Knight('N', 0, 1, 1);
+		dimensions[0][2] = new Bishop('B', 0, 2, 1);
+		dimensions[0][3] = new Queen('Q', 0, 3, 1);
+		dimensions[0][4] = new King('K', 0, 4, 1);
+		dimensions[0][5] = new Bishop('B', 0, 5, 1);
+		dimensions[0][6] = new Knight('N', 0, 6, 1);
+		dimensions[0][7] = new Rook('R', 0, 7, 1);
 
 
 		dimensions[6][0] = new Pawn;
