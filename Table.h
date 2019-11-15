c53bceae85246bca2f511d64af8b449a4763d90e
dany12day <57262666+dany12day@users.noreply.github.com>
2019-11-15 13:28:16 +0200

    Update Table.h
    piesele negre sunt facute

--- Table.h
+++ Table.h
@@ -10,24 +10,6 @@ public:
 				dimensions[i][j] = nullptr;
 
 				dimensions[0][0] = new Rook('r',0,0,0);
-		dimensions[0][1] = new Knight('n',0,1,0);
-		dimensions[0][2] = new Bishop('b',0,2,0);
-		dimensions[0][3] = new Queen('q',0,3,0);
-		dimensions[0][4] = new King('k',0,4,0);
-		dimensions[0][5] = new Bishop('b',0,5,0);
-		dimensions[0][6] = new Knight('n',0,6,0);
-		dimensions[0][7] = new Rook('r',0,7,0);
-
-		dimensions[1][0] = new Pawn('p',1,0,0);
-		dimensions[1][1] = new Pawn('p',1,1,0);
-		dimensions[1][2] = new Pawn('p',1,2,0);
-		dimensions[1][3] = new Pawn('p',1,3,0);
-		dimensions[1][4] = new Pawn('p',1,4,0);
-		dimensions[1][5] = new Pawn('p',1,5,0);
-		dimensions[1][6] = new Pawn('p',1,6,0);
-		dimensions[1][7] = new Pawn('p',1,7,0);
-
-
 		dimensions[6][0] = new Pawn;
 		dimensions[6][1] = new Pawn;
 		dimensions[6][2] = new Pawn;
@@ -45,6 +27,24 @@ public:
 		dimensions[7][5] = new Bishop;
 		dimensions[7][6] = new Knight;
 		dimensions[7][7] = new Rook;
+
+		dimensions[1][0] = new Pawn('P', 1, 0, 1);
+		dimensions[1][1] = new Pawn('P', 1, 1, 1);
+		dimensions[1][2] = new Pawn('P', 1, 2, 1);
+		dimensions[1][3] = new Pawn('P', 1, 3, 1);
+		dimensions[1][4] = new Pawn('P', 1, 4, 1);
+		dimensions[1][5] = new Pawn('P', 1, 5, 1);
+		dimensions[1][6] = new Pawn('P', 1, 6, 1);
+		dimensions[1][7] = new Pawn('P', 1, 7, 1);
+
+		dimensions[0][0] = new Rook('R', 0, 0, 1);
+		dimensions[0][1] = new Knight('N', 0, 1, 1);
+		dimensions[0][2] = new Bishop('B', 0, 2, 1);
+		dimensions[0][3] = new Queen('Q', 0, 3, 1);
+		dimensions[0][4] = new King('K', 0, 4, 1);
+		dimensions[0][5] = new Bishop('B', 0, 5, 1);
+		dimensions[0][6] = new Knight('N', 0, 6, 1);
+		dimensions[0][7] = new Rook('R', 0, 7, 1);
 	}
 
 	void print()
